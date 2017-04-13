42ee714b81e14279b34e7c7df4bf94840e745290
hamidreza <hosseinkhani@live.com>
2017-04-14 02:13:16 +0430

    add "SharedPreferencesHelper" add Auth static helpers in it

---
+++ app/src/main/java/ir/tvnasim/khandevaneh/helper/SharedPreferencesHelper.java
@@ -0,0 +1,23 @@
+package ir.tvnasim.khandevaneh.helper;
+
+/**
+ * Created by hamidreza on 4/14/17.
+ */
+
+public class SharedPreferencesHelper {
+
+    public static void storeAccessToken(String accessToken) {
+    }
+
+    public static String retrieveAccessToken(String accessToken) {
+        return "ACCESS-TOKEN";
+    }
+
+    public static void storeRefreshToken(String refreshToken) {
+
+    }
+
+    public static String retrieveRefreshToken(String refreshToken) {
+        return "ACCESS-TOKEN";
+    }
+}

06c49fe136e69074f98ecc4c6f8996050f9c7eb8
hamidreza <hosseinkhani@live.com>
2017-04-30 00:49:11 +0430

    complete PhoneNoFragment

--- app/src/main/java/ir/tvnasim/khandevaneh/account/login/LoginActivity.java
+++ app/src/main/java/ir/tvnasim/khandevaneh/account/login/LoginActivity.java
@@ -2,13 +2,14 @@ package ir.tvnasim.khandevaneh.account.login;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import ir.tvnasim.khandevaneh.R;
 import ir.tvnasim.khandevaneh.app.BaseActivity;
 
-public class LoginActivity extends BaseActivity {
-
+public class LoginActivity extends BaseActivity implements PhoneNoFragment.OnFragmentInteractionListener {
     public static void start(Context starter) {
         Intent intent = new Intent(starter, LoginActivity.class);
         starter.startActivity(intent);
@@ -18,10 +19,18 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.activityLogin_frameLayout_fragmentContainer, PhoneNoFragment.newInstance()).commit();
+
     }
 
     @Override
     public int getToolbarViewId() {
         return R.layout.toolbar_login;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
